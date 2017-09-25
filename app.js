a3497243dd285ea2468d9d097c96262668fd3c0f
awener <madis.joekaar@gmail.com>
2017-09-25 05:59:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('express')();
 
 
 app.get("/", function(req, res) {
-  return res.status(200).end("deploy?!?");
+  return res.status(200).end("murrdeploy?!?");
 });
 
 console.log("webhook much!");
